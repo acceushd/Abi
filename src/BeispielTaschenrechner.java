13db516d0a4a8de732598e6e0ad2186248203a64
acceushd <117960238+acceushd@users.noreply.github.com>
2023-04-09 22:16:09 +0200

    Beispiel für inhertance hinzugefügt

---
+++ src/BeispielTaschenrechner.java
@@ -0,0 +1,10 @@
+public class BeispielTaschenrechner extends Rechenarten{
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 5;
+        System.out.println(addition(num1,num2));
+        System.out.println(division(num2,num1));
+        System.out.println(multiplication(num1,num2));
+        System.out.println(subtraction(num2,num1));
+    }
+}
